494c8771768d1327163fd25804e6ae478ca91b2c
jiahao kuang <jiahaokuang7066@gmail.com>
2017-12-10 03:57:48 +0000

    FileSystem

---
+++ moviesService/test/FileSystem.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let filesystem = require("../libs/FileSystem.js");
+let fs = require("fs");
+let path = require("path");
+
+
+let folder = path.join(__dirname, 'test/');
+
+console.log(null == undefined);
+console.log(null === undefined);
